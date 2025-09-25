907ab51ce0792e492144fbc087681c75ac9bf2e7
Tomas Pozo <tomaspozogarzon@gmail.com>
2025-09-24 21:23:48 -0500

    Fix migration

--- supabase/migrations/20250925012548_enable_realtime.sql
+++ supabase/migrations/20250925012548_enable_realtime.sql
@@ -1,4 +1,3 @@
 ALTER PUBLICATION supabase_realtime ADD TABLE sensor_readings;
 ALTER PUBLICATION supabase_realtime ADD TABLE room_bookings;  
 ALTER PUBLICATION supabase_realtime ADD TABLE facility_alerts;
-ALTER PUBLICATION supabase_realtime ADD TABLE service_tickets;
