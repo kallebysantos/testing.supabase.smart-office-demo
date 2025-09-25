9dd6aa73b152262a226e266c0626b00b9c77af5d
Tomas Pozo <tomaspozogarzon@gmail.com>
2025-09-24 20:29:22 -0500

    Add realtime setup

---
+++ supabase/migrations/20250925012548_enable_realtime.sql
@@ -0,0 +1,4 @@
+ALTER PUBLICATION supabase_realtime ADD TABLE sensor_readings;
+ALTER PUBLICATION supabase_realtime ADD TABLE room_bookings;  
+ALTER PUBLICATION supabase_realtime ADD TABLE facility_alerts;
+ALTER PUBLICATION supabase_realtime ADD TABLE service_tickets;
